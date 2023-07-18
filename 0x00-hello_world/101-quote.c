752a45d3bb904180a3c940fcfa320a88292c5117
xqzte <bilalidris011@gmail.com>
2023-07-17 17:15:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always return 0 (success)
+ * Return: Always return 1 (success)
  */
 
 int main(void)
 {       
         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
-}
+}   
