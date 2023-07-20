656bc7ba75dda9bf725e491738d0a8f38a8160df
xqzte <bilalidris011@gmail.com>
2023-07-19 21:41:29 -0700

    5-sign.c -o 5-sign

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - short description,
+ *
+ * Return: Description of the returned value
+ */
+int _isalpha(int c)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
