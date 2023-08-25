c18b68e26bc561aac1f86c3616439fce00e6b4d2
xqzte <bilalidris011@gmail.com>
2023-08-25 03:50:12 -0700

    stringly

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void first(void)_attribute_((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
  * first - prints a sentence before the main
- * function is executred
+ * function is executed
  */
 void first(void)
 {
